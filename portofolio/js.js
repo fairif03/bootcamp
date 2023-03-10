d4788caee77fa56dd3aab7de38b7528a271e532f
nurzaman-now <imannj69@gmail.com>
2023-03-10 22:11:05 +0700

    update

--- portofolio/js.js
+++ portofolio/js.js
@@ -1,25 +1,24 @@
-const weatherAPI = 
-"http://api.weatherapi.com/v1/current.json?key=79de7420a3fe40febaf144116230903&aqi=no";
+const weatherAPI =
+    "http://api.weatherapi.com/v1/current.json?key=79de7420a3fe40febaf144116230903&aqi=no";
 const keyword = document.querySelector('.keyword');
 const btnSearch = document.querySelector('.btn-search');
 const container = document.getElementById('container');
 
-btnSearch.onclick = () => {   
-fetch(`${weatherAPI}&q=${keyword.value}`)
-.then((res) => res.json())
-.then((data) => {
-    console.log(data);
-    let element = ''
+btnSearch.onclick = () => {
+    fetch(`${weatherAPI}&q=${keyword.value}`)
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data);
+            let element = ''
 
-    element = showElement(data)
-    container.innerHTML = element;
-    // const current = data.current;
-    // const location = data.location;
-});
+            element = showElement(data)
+            container.innerHTML = element;
+            // const current = data.current;
+            // const location = data.location;
+        });
 }
 
-function showElement(data)
-{
+function showElement(data) {
     return `<h3>${data.location.name},${data.location.region}, ${data.location.country}</h3>
     <div class="box">
         <img src="http:${data.current.condition.icon}" alt="">
